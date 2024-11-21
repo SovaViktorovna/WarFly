0a63ccd9090b05249ad6bd2d29000bc6cfb6c4bc
Viktoria Demchenko <sovaviktorovna430@gmail.com>
2024-11-21 11:21:33 +0400

    Islands movement

---
+++ WarFly/Cloud.swift
@@ -0,0 +1,64 @@
+//
+//  Cloud.swift
+//  WarFly
+//
+//  Created by Виктория Демченко on 20.11.24.
+//
+
+import SpriteKit
+import GameplayKit
+
+protocol GameBackgroundSpriteable{
+    static func populate() -> Self
+    static func randomPoint() -> CGPoint
+}
+
+extension GameBackgroundSpriteable{
+    static func randomPoint() -> CGPoint {
+        let screen = UIScreen.main.bounds
+        let destribution = GKRandomDistribution(lowestValue: Int(screen.size.height) + 100, highestValue: Int(screen.size.height) + 200)
+        let y = CGFloat(destribution.nextInt())
+        let x = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: Int(screen.size.width )))
+        
+        return CGPoint(x: x, y: y)
+    }
+}
+
+final class Cloud: SKSpriteNode, GameBackgroundSpriteable {
+    static func populate() -> Cloud  {
+        let cloudImageName = configureName()
+        let cloud = Cloud(imageNamed: cloudImageName)
+        cloud.setScale(randomScaleFactor)
+        cloud.position = randomPoint()
+        cloud.zPosition = 10
+        cloud.run(move(from: cloud.position ))
+        
+        return cloud
+    }
+    
+    fileprivate static func configureName() -> String {
+        let destribution = GKRandomDistribution(lowestValue: 1, highestValue: 3)
+        let randomNumber = destribution.nextInt()
+        let imageName = "cl" + "\(randomNumber)"
+        
+        return imageName
+    }
+    
+    fileprivate  static var randomScaleFactor: CGFloat {
+        let destribution = GKRandomDistribution(lowestValue: 15, highestValue: 25)
+        let randomNumber = CGFloat(destribution.nextInt()) / 10
+        
+        return randomNumber
+    }
+    
+    //движение объектов вниз
+    fileprivate static func move(from point: CGPoint) -> SKAction {
+        let movePoint = CGPoint(x: point.x, y: -200)
+        let moveDistance = point.y + 200
+        let movementSpeed: CGFloat = 150.0
+        let duration = moveDistance / movementSpeed
+        
+        return SKAction.move(to: movePoint, duration: TimeInterval(duration))
+    }
+}
+
