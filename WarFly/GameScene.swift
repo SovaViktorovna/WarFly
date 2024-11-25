825815e72562ea006aa4adb5a559e340ee8ad4f0
Viktoria Demchenko <sovaviktorovna430@gmail.com>
2024-11-25 13:22:15 +0400

    Create user interface

--- WarFly/GameScene.swift
+++ WarFly/GameScene.swift
@@ -9,7 +9,15 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene{
+    
     var player: PlayerPlane!
+    let scoreBackground = SKSpriteNode(imageNamed: "scores")
+    let scoreLable = SKLabelNode(text: "1000")
+    let menuButton = SKSpriteNode(imageNamed: "menu")
+    let life1 = SKSpriteNode(imageNamed: "life")
+    let life2 = SKSpriteNode(imageNamed: "life")
+    let life3 = SKSpriteNode(imageNamed: "life")
+    
     
     override func didMove(to view: SKView) {
         
@@ -19,13 +27,39 @@ class GameScene: SKScene{
         configureStartScene()
         spawnClouds()
         spawnIslands()
+        self.player.performFly()
         
-        let deadline = DispatchTime.now() + . nanoseconds(1)
-        DispatchQueue.main.asyncAfter(deadline: deadline) { [unowned self] in
-            self.player.performFly()
-        }
         spawnPowerUp()
         spawnEnemies()
+        configureUI()
+    }
+    
+    fileprivate func configureUI() {
+        scoreBackground.position = CGPoint(x: scoreBackground.size.width + 10, y: self.size.height - scoreBackground.size.height / 2 - 10)
+        scoreBackground.anchorPoint = CGPoint(x: 1.0, y: 0.5)
+        scoreBackground.zPosition = 99
+        addChild(scoreBackground)
+        
+        scoreLable.horizontalAlignmentMode = .right
+        scoreLable.verticalAlignmentMode = .center
+        scoreLable.position = CGPoint(x: -10, y: 3)
+        scoreLable.zPosition = 100
+        scoreLable.fontName = "AmericanTypewritter-Bolt"
+        scoreLable.fontSize = 30
+        scoreBackground.addChild(scoreLable)
+        
+        menuButton.position = CGPoint(x: 20, y: 20)
+        menuButton.anchorPoint = CGPoint(x: 0.0, y: 0.0)
+        menuButton.zPosition = 100
+        addChild(menuButton)
+        
+        let lifes = [life1, life2, life3]
+        for (index, life) in lifes.enumerated() {
+            life.position = CGPoint(x: self.size.width - CGFloat(index + 1) * (life.size.width + 3), y: 20)
+            life.zPosition = 100
+            life.anchorPoint = CGPoint(x: 0.0, y: 0.0)
+            addChild(life)
+        }
     }
     
     fileprivate func spawnPowerUp() {
